61b23f31e55ed0487e43bcdfc77592b540e651ba
niquola <niquola@gmail.com>
2025-06-28 11:26:53 +0100

    cleanup

--- migrations/20250628101222_initial_schema.down.sql
+++
@@ -1,5 +0,0 @@
--- Migration: initial_schema
--- Created: 2025-06-28T10:12:22.471Z
--- Down migration (rollback)
-
--- Add your rollback SQL statements here
--- migrations/20250628101222_initial_schema.up.sql
+++
@@ -1,5 +0,0 @@
--- Migration: initial_schema
--- Created: 2025-06-28T10:12:22.471Z
--- Up migration
-
--- Add your SQL statements here
